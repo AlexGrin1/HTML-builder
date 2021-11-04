02cfb353ebabf9a51130fdcfb19fcc54c0941ec9
AlexGrin1 <a_grynevich@mail.ru>
2021-11-04 16:15:04 +0300

    feat: solution task 03

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,12 +1,32 @@
 const fs = require("fs");
+const { resolve } = require("path");
+const fsP = require("fs").promises;
 const path = require("path");
-async function getName() {
-  await fs.readdir(path.join(__dirname, "secret-folder"), function (err, files) {
-    if (err) {
-      console.error("Could not list the directory.", err);
-      process.exit(1);
-    }
-    for (let name of files) console.log(name);
-  });
-}
-getName();
+let nextDir = async (pth) => {
+  for (let file of await fsP.readdir(path.join(__dirname, "secret-folder"))) {
+    fsP.lstat(path.join(__dirname, "secret-folder", file)).then((data) => {
+      if (data.isFile()) {
+        async function getName() {
+          await fs.readdir(path.join(__dirname, "secret-folder"), function (err, files) {
+            if (err) {
+              console.error("Could not list the directory.", err);
+              process.exit(1);
+            }
+            let name = file;
+            const pathToFile = path.join(__dirname, name);
+            const p = path.parse(pathToFile);
+            async function getResult() {
+              fs.stat(`${__dirname}/secret-folder/${p.base}`, (err, stats) => {
+                if (err) throw err;
+                console.log(`${p.name} - ${p.ext.split("").slice(1, -1).join("")} - ${stats.size / 1000} kb`);
+              });
+            }
+            getResult();
+          });
+        }
+        getName();
+      }
+    });
+  }
+};
+nextDir();
